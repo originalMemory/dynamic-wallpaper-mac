259437f161c953720a84657097135ca8939424a1
wuhb <wuhb@yuanfudao.com>
2022-04-15 16:52:34 +0800

    ADD: 所有视频列表 UI 及导入、加载逻辑

--- DynamicWallpaper/ContentView.swift
+++ DynamicWallpaper/ContentView.swift
@@ -8,9 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchTitle: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(alignment: .top) {
+            // 左侧功能栏
+            Divider()
+            VStack(alignment: .leading) {
+                Spacer()
+                Button("所有视频") {}
+                Button("播放列表") {}
+                Spacer()
+            }
+            .padding(.horizontal, 5).frame(width: 150)
+            Divider().background(Color.white)
+            // 图片列表
+            videoView
+            Divider()
+            // 显示器及设置属性
+            VStack {
+                Text("显示器")
+            }
+            .frame(width: 150)
+            Divider()
+        }
+        .frame(width: 1100, height: 800, alignment: .center)
+    }
+
+    private var videos: [Video] = []
+
+    init() {
+        videos = DBManager.share.queryFromDb(fromTable: Table.video) ?? []
+    }
+
+    var videoView: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Button("导入视频") {
+                    selectImportVideoPaths()
+                }
+                TextField("输入搜索条件", text: $searchTitle).frame(width: 100)
+                Button("搜索") {
+                    // TODO: 搜索
+                }
+            }
+            ScrollView {
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 3)) {
+                    ForEach(0..<videos.count, id: \.self) { i in
+                        let video = videos[i]
+                        VideoPreviewView(
+                            vm: VideoPreviewView.ViewModel.fromVideo(video)
+                        ).frame(height: 200)
+                    }
+                }
+            }
+        }
+        .padding(.vertical, 10).frame(maxWidth: .infinity)
+    }
+
+    private func selectImportVideoPaths() {
+        let dialog = NSOpenPanel()
+        dialog.title = "选择视频文件或目录"
+        dialog.showsResizeIndicator = true
+        dialog.allowsMultipleSelection = true
+        dialog.canChooseDirectories = true
+        dialog.allowedContentTypes = [.mpeg4Movie, .directory]
+
+        if dialog.runModal() != NSApplication.ModalResponse.OK {
+            return
+        }
+
+        var filePaths: [String] = []
+        for url in dialog.urls {
+            var directoryExists = ObjCBool(false)
+            FileManager.default.fileExists(atPath: url.path, isDirectory: &directoryExists)
+            if !directoryExists.boolValue {
+                if isVideo(url: url) {
+                    filePaths.append(url.path)
+                }
+                continue
+            }
+            let enumeratorAtPath = FileManager.default.enumerator(atPath: url.path)
+            for obj in enumeratorAtPath?.allObjects ?? [] {
+                guard let path = obj as? String else {
+                    continue
+                }
+                if isVideo(url: URL(fileURLWithPath: path)) {
+                    filePaths.append(url.path.appendPathComponent(path))
+                }
+            }
+            VideoHelper.share.importVideo(filePaths: filePaths)
+        }
+    }
+
+    private func isVideo(url: URL) -> Bool {
+        let videoExtensions = ["mkv", "mp4", "flv", "avi"]
+        return videoExtensions.contains(url.pathExtension.lowercased())
     }
 }
 
